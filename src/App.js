04eec6590f3e66f96af1d6b2086f2a782bd09149
aman <amansoni20020@gmail.com>
2023-09-20 19:39:21 +0530

    updated with react-chatbot

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Chatbot from 'react-chatbot-kit';
+import 'react-chatbot-kit/build/main.css';
+import MessageParser from './MessageParcer';
+import ActionProvider from './ActionProvider';
+import config from './config';
 
 function App() {
   return (
     <div className="App">
+      <Chatbot
+        config={config}
+        messageParser={MessageParser}
+        actionProvider={ActionProvider}
+      />
     </div>
   );
 }
